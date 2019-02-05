875353a037ecdc1105fe99c52bf0c85905b1eb65
Lotanna <sugabelly@gmail.com>
2019-02-05 18:08:56 -0500

    added notes file for debugging

---
+++ JournalCoreData/PerformanceNotes.swift
@@ -0,0 +1,9 @@
+//
+//  PerformanceNotes.swift
+//  JournalCoreData
+//
+//  Created by Lotanna Igwe-Odunze on 2/5/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
