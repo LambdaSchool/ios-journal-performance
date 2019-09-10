8365b00eba55d547478ff5bdfdf11ca0ee3e3476
bradleyyin <bradleyyin@gmail.com>
2019-09-10 16:49:05 -0700

    reinitialize frc to refresh data

--- JournalCoreData/View Controllers/EntriesTableViewController.swift
+++ JournalCoreData/View Controllers/EntriesTableViewController.swift
@@ -17,13 +17,13 @@ class EntriesTableViewController: UITableViewController, NSFetchedResultsControl
         let refreshControl = UIRefreshControl()
         refreshControl.addTarget(self, action: #selector(refresh(_:)), for: .valueChanged)
         self.refreshControl = refreshControl
-        
+        resetFrc()
         refresh(nil)
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
+        resetFrc()
         tableView.reloadData()
     }
     
@@ -39,9 +39,26 @@ class EntriesTableViewController: UITableViewController, NSFetchedResultsControl
             
             DispatchQueue.main.async {
                 self.tableView.reloadData()
+                
                 self.refreshControl?.endRefreshing()
             }
         }
+    }
+    
+    func resetFrc() {
+        self.fetchedResultsController = {
+            let fetchRequest: NSFetchRequest<Entry> = Entry.fetchRequest()
+            fetchRequest.sortDescriptors = [NSSortDescriptor(key: "mood", ascending: false), NSSortDescriptor(key: "timestamp", ascending: false)]
+            
+            let moc = CoreDataStack.shared.mainContext
+            let frc = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: moc, sectionNameKeyPath: "mood", cacheName: nil)
+            
+            frc.delegate = self
+            
+            try! frc.performFetch()
+            
+            return frc
+        }()
     }
     
     // MARK: - Table view data source
