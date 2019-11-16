c320de25893011a24b03d34fcea0f5bf5d49dd0d
John Kouris <jdkouris@gmail.com>
2019-11-16 09:27:24 -0800

    Reseting the context to improve UI load Switched the == to IN within the NSPredicate to improve query time

--- JournalCoreData/Model Controllers/CoreDataImporter.swift
+++ JournalCoreData/Model Controllers/CoreDataImporter.swift
@@ -44,11 +44,10 @@ class CoreDataImporter {
         guard let identifier = identifier else { return nil }
         
         let fetchRequest: NSFetchRequest<Entry> = Entry.fetchRequest()
-        fetchRequest.predicate = NSPredicate(format: "identifier == %@", identifier)
+        fetchRequest.predicate = NSPredicate(format: "identifier IN %@", identifier)
         
         var result: Entry? = nil
         do {
-            result = try context.fetch(fetchRequest).first
         } catch {
             NSLog("Error fetching single entry: \(error)")
         }
