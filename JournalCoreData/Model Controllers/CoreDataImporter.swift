b8351e221b9edeb86836371bdb05c844ccdac0d2
David Wright <davidtwright89@gmail.com>
2020-04-11 02:03:44 -0400

    Optimized sync function

--- JournalCoreData/Model Controllers/CoreDataImporter.swift
+++ JournalCoreData/Model Controllers/CoreDataImporter.swift
@@ -16,10 +16,20 @@ class CoreDataImporter {
     
     func sync(entries: [EntryRepresentation], completion: @escaping (Error?) -> Void = { _ in }) {
         self.context.perform {
+            guard let coreDataEntries = self.fetchAllEntriesFromPersistentStore(in: self.context) else {
+                // Core Data is empty. Save all entries to persistent store
+                for entryRep in entries {
+                    _ = Entry(entryRepresentation: entryRep, context: self.context)
+                }
+                completion(nil)
+                return
+            }
+            
             for entryRep in entries {
                 guard let identifier = entryRep.identifier else { continue }
                 
-                let entry = self.fetchSingleEntryFromPersistentStore(with: identifier, in: self.context)
+                //let entry = self.fetchSingleEntryFromPersistentStore(with: identifier, in: self.context)
+                let entry = coreDataEntries.first { $0.identifier == identifier }
                 if let entry = entry, entry != entryRep {
                     self.update(entry: entry, with: entryRep)
                 } else if entry == nil {
@@ -52,6 +62,18 @@ class CoreDataImporter {
             NSLog("Error fetching single entry: \(error)")
         }
         return result
+    }
+    
+    private func fetchAllEntriesFromPersistentStore(in context: NSManagedObjectContext) -> [Entry]? {
+        let fetchRequest: NSFetchRequest<Entry> = Entry.fetchRequest()
+        
+        var results: [Entry]? = nil
+        do {
+            results = try context.fetch(fetchRequest)
+        } catch {
+            NSLog("Error fetching all entries: \(error)")
+        }
+        return results
     }
     
     let context: NSManagedObjectContext
