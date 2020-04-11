0cdaffea979189d9d5c1d1ed98e02e0f907d9f5e
Jessie <jessie.griffin@icloud.com>
2020-04-10 23:12:17 -0400

    Adds the beginnings of caching

--- JournalCoreData/Models/Cache.swift
+++ JournalCoreData/Models/Cache.swift
@@ -6,4 +6,17 @@
 //  Copyright © 2020 Lambda School. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class Cache<Key: Hashable, Value> {
+    
+    func cache(value: Value, for key: Key) {
+        cache[key] = value
+    }
+    
+    func value(for key: Key) -> Value? {
+        return cache[key]
+    }
+    
+    private var cache = [Key : Value]()
+}
