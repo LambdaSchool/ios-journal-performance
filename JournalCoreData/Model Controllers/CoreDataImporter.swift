f26328d25a81c16ddb9af833098b8fbdada536df
Lydia Zhang <art4everyan@gmail.com>
2020-04-14 19:43:58 -0400

    Bug version: Duplicates

--- JournalCoreData/Model Controllers/CoreDataImporter.swift
+++ JournalCoreData/Model Controllers/CoreDataImporter.swift
@@ -10,26 +10,6 @@ import Foundation
 import CoreData
 
 class CoreDataImporter {
-    init(context: NSManagedObjectContext) {
-        self.context = context
-    }
-    
-    func sync(entries: [EntryRepresentation], completion: @escaping (Error?) -> Void = { _ in }) {
-        
-        self.context.perform {
-            for entryRep in entries {
-                guard let identifier = entryRep.identifier else { continue }
-                
-                let entry = self.fetchSingleEntryFromPersistentStore(with: identifier, in: self.context)
-                if let entry = entry, entry != entryRep {
-                    self.update(entry: entry, with: entryRep)
-                } else if entry == nil {
-                    _ = Entry(entryRepresentation: entryRep, context: self.context)
-                }
-            }
-            completion(nil)
-        }
-    }
     
     private func update(entry: Entry, with entryRep: EntryRepresentation) {
         entry.title = entryRep.title
@@ -39,21 +19,43 @@ class CoreDataImporter {
         entry.identifier = entryRep.identifier
     }
     
-    private func fetchSingleEntryFromPersistentStore(with identifier: String?, in context: NSManagedObjectContext) -> Entry? {
+    func updateEntries(with representations: [EntryRepresentation], completion: @escaping (Error?) -> () = {_ in}) {
         
-        guard let identifier = identifier else { return nil }
+        let entriesWithID = representations.filter({ $0.identifier != nil })
+        
+        let identifiersToFetch = entriesWithID.compactMap({ UUID(uuidString: $0.identifier!) })
+        
+        let representationsByID = Dictionary(uniqueKeysWithValues: zip(identifiersToFetch, entriesWithID))
+        
+        var entriesToCreate = representationsByID
         
         let fetchRequest: NSFetchRequest<Entry> = Entry.fetchRequest()
-        fetchRequest.predicate = NSPredicate(format: "identifier == %@", identifier)
+        fetchRequest.predicate = NSPredicate(format: "identifier IN %@", identifiersToFetch)
         
-        var result: Entry? = nil
-        do {
-            result = try context.fetch(fetchRequest).first
-        } catch {
-            NSLog("Error fetching single entry: \(error)")
+        let context = CoreDataStack.shared.mainContext
+        
+        context.perform{
+            
+            do {
+                let existingEntries = try context.fetch(fetchRequest)
+                
+                for entry in existingEntries {
+                    guard let id = entry.identifier,
+                        let identifier = UUID(uuidString: id),
+                        let representation = representationsByID[identifier] else { continue }
+                    self.update(entry: entry, with: representation)
+                    
+                    entriesToCreate.removeValue(forKey: identifier)
+                }
+                
+                for representation in entriesToCreate.values {
+                    _ = Entry(entryRepresentation: representation, context: context)
+                }
+                completion(nil)
+            } catch {
+                NSLog("Error fetching tasks for UUIDs: \(error)")
+                completion(error)
+            }
         }
-        return result
     }
-    
-    let context: NSManagedObjectContext
 }
