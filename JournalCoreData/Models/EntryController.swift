b3455f8fd8ecf4ec77007b78e06e23d06723dbe5
Kelson Hartle <kelson99hartle@icloud.com>
2020-06-09 16:57:42 -0600

    MVP

--- JournalCoreData/Models/EntryController.swift
+++ JournalCoreData/Models/EntryController.swift
@@ -9,7 +9,7 @@
 import Foundation
 import CoreData
 
-let baseURL = URL(string: "https://journal-performance2.firebaseio.com/")!
+let baseURL = URL(string: "https://journal-testing.firebaseio.com/")!
 
 class EntryController {
         
