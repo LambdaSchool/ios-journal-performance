f5e27724e6bc3e3e4add40bdde8e930cfe898554
nonye <nnezekwo@gmail.com>
2020-06-09 14:49:08 -0700

    Circle of death

--- JournalCoreData/Model Controllers/CoreDataImporter.swift
+++ JournalCoreData/Model Controllers/CoreDataImporter.swift
@@ -13,22 +13,25 @@ class CoreDataImporter {
     init(context: NSManagedObjectContext) {
         self.context = context
     }
-    
+    // issues
     func sync(entries: [EntryRepresentation], completion: @escaping (Error?) -> Void = { _ in }) {
-        
         self.context.perform {
+            let identifiers = entries.compactMap ({ $0.identifier })
+            let myEntries = self.fetchEntriesFromPersistentStore(with: identifiers, in: self.context)
+            
+            
             for entryRep in entries {
                 guard let identifier = entryRep.identifier else { continue }
                 
-                let entry = self.fetchSingleEntryFromPersistentStore(with: [identifier], in: self.context)
-                if let entry = entry, entry != entryRep {
+                if let entry = myEntries[identifier] {
                     self.update(entry: entry, with: entryRep)
-                } else if entry == nil {
+                } else {
                     _ = Entry(entryRepresentation: entryRep, context: self.context)
                 }
             }
             completion(nil)
         }
+        
     }
     
     private func update(entry: Entry, with entryRep: EntryRepresentation) {
@@ -38,18 +41,23 @@ class CoreDataImporter {
         entry.timestamp = entryRep.timestamp
         entry.identifier = entryRep.identifier
     }
-    
-    private func fetchSingleEntryFromPersistentStore(with identifiers: [String], in context: NSManagedObjectContext) -> Entry? {
+    // This is where most changes need to happen. Slow.
+    // Changed from single entry to all since it was not populating all my entries, loading circle of death
+    private func fetchEntriesFromPersistentStore(with identifiers: [String], in context: NSManagedObjectContext) -> [String : Entry] {
         
-//        guard let identifier = identifier else { return nil }
+        //        guard let identifier = identifier else { return nil }
         
         let fetchRequest: NSFetchRequest<Entry> = Entry.fetchRequest()
         // changed
         fetchRequest.predicate = NSPredicate(format: "identifier IN %@", identifiers)
         
-        var result: Entry? = nil
+        var result = [String : Entry]()
         do {
-            result = try context.fetch(fetchRequest).first
+            result = try context.fetch(fetchRequest).reduce(into: [String : Entry]()) {
+                // $1 represents the second parameter, $0 is the first parameter
+                guard let id = $1.identifier else { return }
+                $0[id] = $1
+            }
         } catch {
             NSLog("Error fetching single entry: \(error)")
         }
@@ -57,4 +65,5 @@ class CoreDataImporter {
     }
     
     let context: NSManagedObjectContext
+    
 }
