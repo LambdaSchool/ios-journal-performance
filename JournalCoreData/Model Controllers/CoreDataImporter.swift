66af55aeb876c6ba1f61c294ecf091908d2a2e57
Clayton Watkins <watkinclayton17@yahoo.com>
2020-07-24 14:12:44 -0700

    Optimized syncing

--- JournalCoreData/Model Controllers/CoreDataImporter.swift
+++ JournalCoreData/Model Controllers/CoreDataImporter.swift
@@ -17,11 +17,16 @@ class CoreDataImporter {
     func sync(entries: [EntryRepresentation], completion: @escaping (Error?) -> Void = { _ in }) {
        
         self.context.perform {
-            print("Started Sync \(Date())")
+            guard let coreDataEntries = self.fetchAllEntriesFromPersistentStore(in: self.context) else {
+                for entryRep in entries{
+                    _ = Entry(entryRepresentation: entryRep, context: self.context)
+                }
+                completion(nil)
+                return
+            }
             for entryRep in entries {
                 guard let identifier = entryRep.identifier else { continue }
-                
-                let entry = self.fetchSingleEntryFromPersistentStore(with: identifier, in: self.context)
+                let entry = coreDataEntries.first { $0.identifier == identifier }
                 if let entry = entry, entry != entryRep {
                     self.update(entry: entry, with: entryRep)
                 } else if entry == nil {
@@ -41,21 +46,18 @@ class CoreDataImporter {
         entry.identifier = entryRep.identifier
     }
     
-    private func fetchSingleEntryFromPersistentStore(with identifier: String?, in context: NSManagedObjectContext) -> Entry? {
-        
-        guard let identifier = identifier else { return nil }
-        
-        let fetchRequest: NSFetchRequest<Entry> = Entry.fetchRequest()
-        fetchRequest.predicate = NSPredicate(format: "identifier == %@", identifier)
-        
-        var result: Entry? = nil
-        do {
-            result = try context.fetch(fetchRequest).first
-        } catch {
-            NSLog("Error fetching single entry: \(error)")
-        }
-        return result
-    }
+    private func fetchAllEntriesFromPersistentStore(in context: NSManagedObjectContext) -> [Entry]? {
+         let fetchRequest: NSFetchRequest<Entry> = Entry.fetchRequest()
+
+         var results: [Entry]? = nil
+         do {
+             results = try context.fetch(fetchRequest)
+         } catch {
+             NSLog("Error fetching all entries: \(error)")
+         }
+         return results
+     }
+
     
     let context: NSManagedObjectContext
 }
